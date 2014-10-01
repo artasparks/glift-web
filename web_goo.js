06a62648268630f4a34a9ba06e666bcca886895f
kashomon <jrhoak@gmail.com>
2014-09-30 21:54:00 -0600

    Glift at 0.19.1

--- web_goo.js
+++ web_goo.js
@@ -26,9 +26,8 @@ var getContent = function(hash) {
           sgf: {
             sgfString: "(;FF[4]GM[1]SZ[19]CA[UTF-8]SO[Glift]EV[]PB[Chen Yaoye]BR[9p]PW[Tan Xiao]WR[7p]KM[7.5]DT[2013-11-18]RE[B+0.5];B[qd];W[dc];B[pq];W[dq];B[nc];W[co];B[qk];W[jp];B[de];W[cg];B[cc];W[gl];B[dd];W[db];B[fd];W[hc];B[cf];W[dh];B[cb];W[mp];B[po];W[kc];B[dk];W[bf];B[be];W[df];B[ce];W[dm];B[fk];W[gh];B[hk];W[gn];B[hq];W[jq];B[eq];W[dr];B[io];W[im];B[jo];W[ko];B[jm];W[jn];B[in];W[kn];B[hm];W[il];B[hl];W[od];B[jk];W[oc])",
             widgetType: 'EXAMPLE',
-            icons: [],
             initialPosition: 50,
-            componentsToUse: ['BOARD']
+            uiComponents: ['BOARD']
           },
           display: {
             disableZoomForMobile: true
